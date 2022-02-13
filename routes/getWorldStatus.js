88c725dd66aab9c76859e29f0607bfc1cad66f46
Ricardo <ricardo.machado.nwi@gmail.com>
2022-02-13 00:03:44 -0300

    Increment Block and autobalance difficult

--- routes/getWorldStatus.js
+++ routes/getWorldStatus.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 // import { Response , Request, Router } from '@types/express'
 // const Router = require('express')
 // const config = require('dotenv')
+import { differenceInDays } from 'date-fns'
 import { config } from 'dotenv'
 import { WorldStatus } from '../models/worldStatus.js'
 
@@ -24,8 +25,46 @@ router.get('/', async (req, res) => {
 
         console.log('THIS IS THE ID', id)
 
-        const newWordStatus = {
-          currentPlayers: worldStatus[0].currentPlayers + 1,
+        let currentPlayers = worldStatus[0].currentPlayers + 1
+
+        let newWordStatus = {}
+
+        if (currentPlayers >= worldStatus[0].targetPlayers) {
+          currentPlayers = 0
+          let targetPlayers = worldStatus[0].targetPlayers
+
+          const currentBlock = worldStatus[0].currentBlock + 1
+          const lastBlockActivation = worldStatus[0].blockActivation
+
+          const currentDate = new Date()
+          const blockActivation = currentDate
+
+          const daysInterval = Math.max(
+            differenceInDays(currentDate, lastBlockActivation),
+            1
+          )
+
+          console.log(
+            'CURRENT DATE',
+            currentDate,
+            'LAST BLOCK DATE',
+            lastBlockActivation
+          )
+          console.log('DAYS INTERVALS', daysInterval)
+          const difficultyBalance = targetPlayers / daysInterval
+          targetPlayers = Math.floor(targetPlayers / daysInterval) * 10
+
+          newWordStatus = {
+            targetPlayers,
+            currentPlayers,
+            currentBlock,
+            blockActivation,
+            difficultyBalance,
+          }
+        } else {
+          newWordStatus = {
+            currentPlayers,
+          }
         }
 
         console.log('NEW WORDL STATUS', newWordStatus)
